a5b7687821d271f62e552d5bb121f447bc3362f1
Christopher Okokon Ntuk <engchris95@gmail.com>
2021-05-09 21:21:31 +0100

    Added the dashboard design

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,47 @@
 <template>
   <div>
-    <Navigation v-if="$route.name !== 'Login' && $route.name !== 'Register'"/>
-    <router-view />
-    <FooterComponent v-if="$route.name !== 'Login' && $route.name !== 'Register'"/>
+    <div v-if="!dashboardRoute">
+      <Navigation v-if="currentRoute !== 'Login' && currentRoute !== 'Register'"/>
+      <router-view />
+      <FooterComponent v-if="currentRoute !== 'Login' && currentRoute !== 'Register'"/>
+    </div>
+    <div v-else>
+      <side-bar />
+      <section class="main_content dashboard_part">
+        <header-section />
+        <div class="main_content_iner">
+          <router-view />
+        </div>
+        <footer-section />
+      </section>
+    </div>
   </div>
 </template>
 <script>
 import FooterComponent from "@/components/_partials/Footer";
 import Navigation from "@/components/_partials/Navigation";
+import SideBar from '@/views/Dashboard/_partials/Sidebar.vue';
+import HeaderSection from '@/views/Dashboard/_partials/Header.vue';
+import FooterSection from '@/views/Dashboard/_partials/Footer.vue';
 export default {
   name: "App",
   components: {
     FooterComponent,
     Navigation,
+    SideBar,
+    HeaderSection,
+    FooterSection
+  },
+  computed: {
+    currentRoute() {
+      return this.$route.name
+    },
+    dashboardRoute() {
+      return this.$route.meta.requiresAuth
+    }
+  },
+  data() {
+    return {}
   },
 };
 </script>
---
+++ src/views/Dashboard/_partials/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="footer_part">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="footer_iner text-center">
+            <p>
+              2021 © Bizpotta - Designed by
+              <a href="#"> <i class="ti-heart"></i> </a
+              ><a href="#"> Dashboard</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="">
+</style>
